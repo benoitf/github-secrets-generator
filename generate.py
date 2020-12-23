b9ce2c3359230d0b432c02c9d16c52d0e80462fb
nickboldt <nboldt@redhat.com>
2020-12-23 09:20:32 -0500

    make github-secrets-generator more generic so it can handle BOTH user/pwd pairs and single secrets too
    Change-Id: I6383f6c3d948f14c5bca4c58e0f8373d44f9cd63
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- generate.py
+++ generate.py
@@ -4,8 +4,10 @@ import json
 import sys
 
 key = sys.argv[1]
-login = sys.argv[2]
-password = sys.argv[3]
+# first secret could be a login
+firstSecret = sys.argv[2]
+# second secret could be a password, ot omitted if only using a key/token
+secondSecret = sys.argv[3]
 
 def encrypt(public_key: str, secret_value: str) -> str:
     """Encrypt a Unicode string using the public key."""
@@ -14,13 +16,16 @@ def encrypt(public_key: str, secret_value: str) -> str:
     encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
     return b64encode(encrypted).decode("utf-8")
 
-encodedLogin = encrypt(key, login)
-encodedPassword = encrypt(key, password)
-
-result = {
-  "login": encodedLogin,
-  "password": encodedPassword
-}
+encodedfirstSecret = encrypt(key, firstSecret)
+if secondSecret:
+  result = {
+    "firstSecret": encodedfirstSecret,
+    "secondSecret": encrypt(key, secondSecret)
+  }
+else:
+  result = {
+    "firstSecret": encodedfirstSecret
+  }
 
 jsonStr = json.dumps(result, indent=2)
 print(jsonStr)
--- generate.sh
+++ generate.sh
@@ -1,26 +1,36 @@
 #!/bin/sh
 GITHUB_TOKEN=$1
 GITHUB_REPO=$2
-LOGIN=$3
-PASSWORD=$4
+# FIRST_VAR could be "QUAY_USERNAME" and FIRST would be the value of that variable
+FIRST_VAR=$3
+FIRST=$4
+# SECOND_VAR could be "QUAY_PASSWORD" and SECOND would be the value of that variable
+# if saving a single secret, omit SECOND_VAR and SECOND value
+SECOND_VAR=$5
+SECOND=$6
 
 OUTPUT=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/${GITHUB_REPO}/actions/secrets/public-key)
 PUBLIC_KEY=$(echo $OUTPUT  | jq -r .key )
 KEY_ID=$(echo $OUTPUT  | jq -r .key_id )
 echo "Generating tokens for repository ${GITHUB_REPO} using the key ${PUBLIC_KEY}"
-ENCRYPT_RESULT=$(python /generate.py ${PUBLIC_KEY} ${LOGIN} ${PASSWORD})
+if [ ${SECOND} ] && [ ${SECOND_VAR} ]; then
+    ENCRYPT_RESULT=$(python /generate.py ${PUBLIC_KEY} ${FIRST} ${SECOND})
+else
+    ENCRYPT_RESULT=$(python /generate.py ${PUBLIC_KEY} ${FIRST})
+fi
 
 #echo ${ENCRYPT_RESULT}
-ENCODED_LOGIN=$(echo ${ENCRYPT_RESULT}  | jq -r .login )
-ENCODED_PASSWORD=$(echo ${ENCRYPT_RESULT}  | jq -r .password )
+ENCODED_FIRSTSECRET=$(echo ${ENCRYPT_RESULT}  | jq -r .firstSecret )
+ENCODED_SECONDSECRET=$(echo ${ENCRYPT_RESULT}  | jq -r .secondSecret )
 
 echo "KEY_ID is ${KEY_ID}"
 
 echo "secrets list [before]:"
 curl -s -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/${GITHUB_REPO}/actions/secrets
 
-curl -H "Authorization: token ${GITHUB_TOKEN}"  -X PUT -H "Content-Type: application/json" -d "{\"encrypted_value\":\"${ENCODED_LOGIN}\",\"key_id\":\"${KEY_ID}\"}" https://api.github.com/repos/${GITHUB_REPO}/actions/secrets/DOCKERHUB_USERNAME
-curl -H "Authorization: token ${GITHUB_TOKEN}"  -X PUT -H "Content-Type: application/json" -d "{\"encrypted_value\":\"${ENCODED_PASSWORD}\",\"key_id\":\"${KEY_ID}\"}" https://api.github.com/repos/${GITHUB_REPO}/actions/secrets/DOCKERHUB_PASSWORD
-
+curl -H "Authorization: token ${GITHUB_TOKEN}"  -X PUT -H "Content-Type: application/json" -d "{\"encrypted_value\":\"${ENCODED_FIRSTSECRET}\",\"key_id\":\"${KEY_ID}\"}" https://api.github.com/repos/${GITHUB_REPO}/actions/secrets/${FIRST_VAR}
+if [ "${ENCODED_SECONDSECRET}" ]; then
+    curl -H "Authorization: token ${GITHUB_TOKEN}"  -X PUT -H "Content-Type: application/json" -d "{\"encrypted_value\":\"${ENCODED_SECONDSECRET}\",\"key_id\":\"${KEY_ID}\"}" https://api.github.com/repos/${GITHUB_REPO}/actions/secrets/${SECOND_VAR}
+fi
 echo "secrets list [after]:"
 curl -s -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/${GITHUB_REPO}/actions/secrets
