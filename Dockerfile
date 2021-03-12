99e268d72864c3ad306922349ee77031f60e0195
nickboldt <nboldt@redhat.com>
2021-03-12 14:04:16 -0500

    add ability to check if a secret already exists; move method pasted into readme to a bash script for easier use
    Change-Id: I1b4fa97f138855d05e505c11f56d67580d71d05b
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.8.2-alpine
 RUN apk add --no-cache bash jq libc-dev libffi-dev make curl py-pip python-dev gcc linux-headers && pip install pynacl
-COPY generate.* /
+# also include checkIfSecretExists.sh but don't use it by default (optional entrypoint)
+COPY checkIfSecretExists.* generate.* /
 ENTRYPOINT [ "/generate.sh" ]
-
---
+++ checkIfSecretExists.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+GITHUB_TOKEN=$1
+GITHUB_REPO=$2
+FIRST_VAR=$3
+
+OUTPUT=$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/${GITHUB_REPO}/actions/secrets/public-key)
+# PUBLIC_KEY=$(echo $OUTPUT  | jq -r .key )
+KEY_ID=$(echo $OUTPUT  | jq -r .key_id )
+# echo "KEY_ID is ${KEY_ID}"
+if [ ! $KEY_ID ]; then echo "Error: could not read public key from https://api.github.com/repos/${GITHUB_REPO}/actions/secrets/public-key !"; exit 1; fi
+
+# echo "Fetching tokens for repository ${GITHUB_REPO} using the key ${PUBLIC_KEY}"
+
+echo -n "Number of secrets: "
+curl -sSL -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/${GITHUB_REPO}/actions/secrets | jq '.total_count'
+
+echo "Secret ${FIRST_VAR}:"
+curl -sSL -H "Authorization: token ${GITHUB_TOKEN}"  -X GET -H "Accept: application/vnd.github.v3+json" \
+ https://api.github.com/repos/${GITHUB_REPO}/actions/secrets/${FIRST_VAR}
